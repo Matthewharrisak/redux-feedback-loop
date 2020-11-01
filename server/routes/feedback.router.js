c91b636084121d47990b224b2d1b753f3b3d7c57
Matthew Harris <matthewharrisak@gmail.com>
2020-11-01 16:25:18 -0600

    added post request to routes folder

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -13,4 +13,19 @@ router.get('/' , (req, res) => {
     });
 })
 
+router.post('/' , (req, res) => {
+let feedback = req.body;
+console.log('posting feedback!!' , feedback);
+let queryText = `INSERT INTO "feedback" ("feeling", "understanding" , "support" , "comments")
+                VALUES ($1, $2, $3, $4);`;
+pool.query(queryText, [feedback.feeling, feedback.understanding,
+                     feedback.support, feedback.comments])
+                     .then(result => {
+                         res.sendStatus(201);
+                     }).catch(error => {
+                         console.log('error in postRequest' , error);
+                         res.sendStatus(500);
+                     });
+});
+
 module.exports = router;
