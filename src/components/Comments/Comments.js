86ff3b934f94350a6cd67a115dc8aa2093d186d0
Matthew Harris <matthewharrisak@gmail.com>
2020-11-01 07:45:15 -0600

    connected comments component to reducer, all inputs are connected and logging from the reducer

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
+import { connect } from 'react-redux';
 
 class Comments extends Component {
+
+  state = {
+    comments: '',
+  }
 
     nextPage = () => {
         console.log('are we clicking?');
         this.props.history.push('/ReviewFeedback');
+        this.addComments();
       }
+
+      addComments = () => {
+        console.log('this is where were adding COMMENTS to the object wow!!' , this.handleChange);
+        this.props.dispatch({
+          type:'FEEDBACK_OBJECT', payload: this.state.comments})      
+          console.log('from addUNDERSTANDING' , this.state);
+           }
+           handleChange = (event) => {
+            this.setState({
+              comments: [event.target.value]
+            });
+          }
 
   render() {
     return (
       <div className="Comments">
       
         <h1>Any comments you want to leave?</h1>
-        <input type="text" placeholder="Insert text here"></input>
+        <input onChange={this.handleChange} type="text" placeholder="Insert text here"></input>
         <button onClick={this.nextPage}> Next Page </button>
         <br/>
       </div>
@@ -21,4 +39,8 @@ class Comments extends Component {
   }
 }
 
-export default Comments;
+
+const putReduxStateOnProps = (reduxState) => ({
+  reduxState:reduxState
+  });
+export default connect(putReduxStateOnProps)(Comments);
